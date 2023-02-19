1f09492131f03d274e7276bf067d2f7058da288e
fr1t2 <fr1t2@protonmail.com>
2023-02-19 09:04:00 -0700

    setup.py versioneer updates and .gitignore

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
-
 import versioneer
-setup(  version=versioneer.get_version(),
-        cmdclass=versioneer.get_cmdclass(), )
+#from setuptools import setup, find_packages
+from distutils.core import setup
+
+setup(
+        name='qrl-chain-scrape',
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+        description='QRL Chain Scrape utility',
+        author='James Gordon',
+        author_email='fr1t2@protonmail.com',
+        #packages=['distutils', 'distutils.command'],
+     )
+
+
+
