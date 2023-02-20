ff6d24c3e7ef49fd0636764c36d112b47b706f4a
fr1t2 <fr1t2@protonmail.com>
2023-02-20 10:13:46 -0700

    address testing and error response

--- src/chainfunctions/address.py
+++ src/chainfunctions/address.py
@@ -66,7 +66,7 @@ def get_address_balance(address):
     try:
         payload = {"address": address}  # using the given address
         get_balance = requests.post("http://127.0.0.1:5359/api/GetBalance", json=payload)
-        print('Address {} balance response {}'.format(address, get_balance.text))
+        print('Address {} balance response {}'.format(address, get_balance.json))
         get_balance.raise_for_status()  # raise an exception if the request fails
     except requests.exceptions.RequestException as err:
         logging.error('Could not get address balance: {}'.format(err))
@@ -74,7 +74,7 @@ def get_address_balance(address):
 
     # get the address balance from the response
     #address_balance = get_balance.json()['balance']  # get the address balance from the response
-
+    address_balance = get_balance.json
     return address_balance  # return the address balance
 
 
