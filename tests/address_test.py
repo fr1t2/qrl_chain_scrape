2350f8698626b5c589d6358942eee85bb780ea33
fr1t2 <fr1t2@protonmail.com>
2023-02-20 10:27:34 -0700

    address testing and error response

--- tests/address_test.py
+++ tests/address_test.py
@@ -8,31 +8,10 @@ def test_get_address_balance():
     """
     test the get_address_balance fuction from address.py
     """
+    # this should pass as the address is valid
     assert get_address_balance('Q010500dacbf29a83ef6832bcf16f0592adb15313836228a873a7b8eed1c354c4414a206ad38728') # valid address
-    assert get_address_balance('invalid_address') # invalid address
+    # this should fail as the address is invalid
+    with pytest.raises(Exception):
+     get_address_balance('invalid_address') # invalid address
 
 
-"""
-with this function:
-
-def test_get_address_balance():
-    test the get_address_balance fuction from address.py
-    assert get_address_balance('Q010500dacbf29a83ef6832bcf16f0592adb15313836228a873a7b8eed1c354c4414a206ad38728') == True # valid address
-    assert get_address_balance('invalid_address') == False # invalid address
-    
-
-Im getting this error:
-pylint: warning
-C0121 - Comparison 'get_address_balance('Q010500dacbf29a83ef6832bcf16f0592adb15313836228a873a7b8eed1c354c4414a206ad38728') == True' should be 
-'get_address_balance('Q010500dacbf29a83ef6832bcf16f0592adb15313836228a873a7b8eed1c354c4414a206ad38728') is True' if checking for the singleton value True, 
-or 'get_address_balance('Q010500dacbf29a83ef6832bcf16f0592adb15313836228a873a7b8eed1c354c4414a206ad38728')' if testing for truthiness (singleton-comparison)
-"""
-
-
-
-def test_get_address_ots_keys():
-    """
-    test the get_address_ots_keys fuction from address.py
-    """
-    assert get_address_ots_keys('Q010500dacbf29a83ef6832bcf16f0592adb15313836228a873a7b8eed1c354c4414a206ad38728') == True
-    assert get_address_ots_keys('invalid_address') == False
