0531cf6ce1227f1e846a1b146404ac2f7799bfe4
fr1t2 <fr1t2@protonmail.com>
2023-02-20 10:44:49 -0700

    rework functions to handle errors correctly

--- tests/address_test.py
+++ tests/address_test.py
@@ -26,3 +26,26 @@ def test_check_address_valid():
     # this should fail as the address is invalid
     with pytest.raises(Exception):
         check_address_valid('invalid_address') # invalid address
+
+
+def test_get_address_ots_keys():
+    """
+    test the get_address_ots_keys fuction from address.py
+    """
+    # this should pass as the address is valid
+    assert get_address_ots_keys('Q010500dacbf29a83ef6832bcf16f0592adb15313836228a873a7b8eed1c354c4414a206ad38728') # valid address
+    # this should fail as the address is invalid
+    with pytest.raises(Exception):
+        get_address_ots_keys('invalid_address') # invalid address
+
+
+def test_get_address_tx_hashes():
+    """
+    test the get_address_tx_hashes fuction from address.py
+    """
+    # this should pass as the address is valid
+    assert get_address_tx_hashes('Q010500dacbf29a83ef6832bcf16f0592adb15313836228a873a7b8eed1c354c4414a206ad38728') # valid address
+    # this should fail as the address is invalid
+    with pytest.raises(Exception):
+        get_address_tx_hashes('invalid_address') # invalid address
+
