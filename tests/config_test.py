ba5d2857714861382245742e8e0e6e9766963af6
fr1t2 <fr1t2@protonmail.com>
2023-02-20 23:56:31 -0700

    testing config

--- tests/config_test.py
+++ tests/config_test.py
@@ -23,35 +23,8 @@ def test_example_config_file_opens():
     except:
         pytest.fail("Failed to open example config file")
 
-def test_config_file_not_empty():
-    """Ensure the file is not empty"""
-    config_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'qrl_chain_state', 'src', 'config.ini'))
-    assert os.stat(config_file).st_size > 0, "Config file is empty"
-
 def test_config_file_is_ini():
     """Check that ist is a vaild .ini file"""
     config_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'qrl_chain_state', 'src', 'config.ini'))
     assert os.path.splitext(config_file)[1] == '.ini', "Config file is not named as a valid .ini file"
 
-def test_all_sections_and_keys_are_present():
-    """compares the example config with the user provided one"""
-
-    config_file = os.path.join(os.path.dirname(__file__), 'config.ini')
-    example_config_file = os.path.join(os.path.dirname(__file__), 'config.ini.example')
-
-    # copy config.ini.example to config.ini if it doesn't exist
-    if not os.path.exists(config_file):
-        shutil.copyfile(example_config_file, config_file)
-
-    config = configparser.ConfigParser()
-    config.read(config_file)
-    ex_conf = configparser.ConfigParser()
-    ex_conf.read(example_config_file)
-
-    for section in ex_conf.sections():
-        if section not in config:
-            pytest.fail(f"Section [{section}] is missing from {config_file}")
-        else:
-            for key in ex_conf[section]:
-                if key not in config[section]:
-                    pytest.fail(f"Key {key} is missing from section [{section}] in {config_file}")
