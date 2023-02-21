4582a12d82f7818cc3104d689085bb7535dae770
fr1t2 <fr1t2@protonmail.com>
2023-02-20 17:49:18 -0700

    Unit test issues, array parse

--- tests/block_test.py
+++ tests/block_test.py
@@ -18,7 +18,8 @@ def test_get_chain_height():
 def test_get_good_block_data(): 
     """ Test the get_block_data() function.
     """
-    block_data_recieved = get_block_data(15)
+    block_height = 15
+    block_data_recieved = get_block_data(block_height)
     assert block_data_recieved is not None
     assert block_data_recieved != {}
     assert block_data_recieved != []
@@ -33,7 +34,7 @@ def test_get_good_block_data():
     assert block_data_recieved != "True"
     assert block_data_recieved != "None"
     # assert that it is 15
-    assert int(block_data_recieved['block_height']) == 15
+    assert int(block_data_recieved['block']['block_number']) == block_height
 
 
     #assert get_block_data(block_height) is not None
@@ -51,7 +52,7 @@ def test_get_good_block_data():
     #assert get_block_data(block_height) != "None"
 
     # assert that it is 15
-    assert int(get_block_data(block_height)['block_number']) == 15
+    assert int(get_block_data(block_height)['block']['block_number']) == 15
 
     # assert that it is a dict
     assert isinstance(get_block_data(block_height), dict)
