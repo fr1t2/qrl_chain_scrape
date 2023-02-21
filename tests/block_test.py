cbc3cb50de9bd361fd41ec65e2503269ed18b369
fr1t2 <fr1t2@protonmail.com>
2023-02-20 17:37:34 -0700

    Unit tests for block.py

--- tests/block_test.py
+++ tests/block_test.py
@@ -1,65 +1,52 @@
 """write tests for the functions in the ../chainfunctions/block.py module"""
 
-# using pytest 
-# https://docs.pytest.org/en/latest/
-# https://docs.pytest.org/en/latest/getting-started.html#getstarted
 
-# import the functions to be tested
-from src.chainfunctions.block import get_chain_height, get_block_data
-
-# import the requests module
-import requests
-
-# import the logging module
-import logging
-
-# import the config module
-from src import config
-
-# import the pytest module
 import pytest
 
-#no
+# import the functions to be tested
+from src import get_chain_height
+from src import get_block_data
 
-# Access the 'walletd' section of the configuration file
-API_URL = config.get('walletd', 'url')
-API_PORT = config.get('walletd', 'port')
-#{API_URL}:{API_PORT}
+
 
 # get the block height of the chain from the local node and return it
 def test_get_chain_height():
     """ Test the get_chain_height() function.
     """
-    # get the block height from the local node
-    try:
-        response = requests.get(f'{API_URL}:{API_PORT}/api/GetHeight') # 5359 is the default port for the QRL walletd-rest-proxy API
-        response.raise_for_status()
-    except requests.exceptions.RequestException as err:
-        logging.error('Could not get chain height: {}'.format(err))
-        raise Exception('Could not get chain height')
+    assert get_chain_height() > 0
 
-        # test for empty array {} and raise exception if it is
-        if response.json() == {}:
-            logging.error('No data returned from the node')
-            raise Exception('No data returned from the node')
+def test_get_good_block_data(): 
+    """ Test the get_block_data() function.
+    """
+    block_height = 15
+    assert get_block_data(block_height) is not None
+    assert get_block_data(block_height) != {}
+    assert get_block_data(block_height) != []
+    assert get_block_data(block_height) != ""
+    assert get_block_data(block_height) != 0
+    assert get_block_data(block_height) != 0.0
+    assert get_block_data(block_height) is not False
+    assert get_block_data(block_height) is not True
+    assert get_block_data(block_height) != "0"
+    assert get_block_data(block_height) != "0.0"
+    assert get_block_data(block_height) != "False"
+    assert get_block_data(block_height) != "True"
+    assert get_block_data(block_height) != "None"
 
-    # get the block height from the response
-    return response.json()['height'] # return the height of the chain
+    # assert that it is 15
+    assert get_block_data(block_height)['block_number'] == 15
 
-block_height = 15
+    # assert that it is a dict
+    assert isinstance(get_block_data(block_height), dict)
 
-# get the block data from the local node for a given block and return it's data in an array
-def test_get_good_block_data(block_height):
+
+
+def test_get_bad_block_data(): 
     """ Test the get_block_data() function.
     """
     # get the block data from the local node
-    try:
-        payload = { "block_number": block_height} # using the given block number
-        block_by_number = requests.post(f'{API_URL}:{API_PORT}api/GetBlockByNumber', json=payload) # 5359 is the default port for the QRL walletd-rest-proxy API
-        block_by_number.raise_for_status() # raise an exception if the request fails
-    except requests.exceptions.RequestException as err:
-        logging.error('Could not get block data: {}'.format(err))
-        raise Exception('Could not get block data')
-    # get the block data from the response
-    block_data = block_by_number.json()['block'] # get the block data from the response
-    return block_data # return the block data in an array
+    block_height = 0xff
+
+    # assert that we get an exception searching for a block that does not exist using 0xff as the block number
+    with pytest.raises(Exception):
+        get_block_data(block_height)
