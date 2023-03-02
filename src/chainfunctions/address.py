e7b339a2bcb2cf581c8cc1d05164f4dbbbf2c19d
fr1t2 <fr1t2@protonmail.com>
2023-03-02 10:48:17 -0700

    Import cleanup

--- src/chainfunctions/address.py
+++ src/chainfunctions/address.py
@@ -16,7 +16,7 @@ Functions:
 import logging
 import requests
 
-from chainfunctions.config import *
+from chainfunctions import config
 
 # Access the 'walletd' section of the configuration file
 API_URL = config.get('walletd', 'url')
